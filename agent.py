1a39b472e8bb334af16e43769e681024dae89e11
prhys <prhys@tutanota.com>
2019-01-02 13:51:41 +0100

    Created movements list and improved mutate function

--- agent.py
+++ agent.py
@@ -1,17 +1,47 @@
 #!/usr/bin/env python
 
+from random import random, randint
+
 class Agent:
-	def __init__(self):
-	    self.agent='Agent'
+	def __init__(self, length):
+		self.movements = []
+
+		for i in range(length):
+			self.append_new_value()
+
+	def append_new_value(self):
+		"""Append a new value to the movements list"""
+		if random() < 0.5:
+			#The appended value will be True or False depending on random()
+			self.movements.append(True)
+		else:
+			self.movements.append(False)
 
 	@property
 	def fitness(self):
 		fitness=-1
 		return fitness
 
-	def mutate(self):
+	def mutate(self, mutation_rate=0.15):
 		"""The agent changes in a random fashion"""
-		pass
+		if random() <= mutation_rate:
+			#Only proceed if random() is lower than the mutation rate
+			if random() < 0.5:
+				self.append_new_value()
+			else:
+				#We didn't append anything to the list, so we will change randomly
+				#one value of the list
+				try:
+					#randint() will raise an error if len(self.movements)==0
+					#In this case, there's nothing to change.
+					position=randint(0, len(self.movements)-1)
+				except ValueError:
+					pass
+				else:
+					if self.movements[position]:
+						self.movements[position]=False
+					else:
+						self.movements[position]=True
 
 	@staticmethod
 	def crossbred(agents):
@@ -53,4 +83,4 @@ def birth(agents):
 
 
 if __name__=='__main__':
-	print(Agent().agent)
+	print(Agent(8).movements)
